b895337fffd9a07775f7df341bd7cbcef97c1eb8
PalstarKe <111015374+PalstarKe@users.noreply.github.com>
2022-12-21 22:54:38 +0300

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,7 +16,7 @@ const sendEmail = async (options) => {
 
   // 2) Define email options (like from, to, subject, email content)
   const mailOpts = {
-    from: 'AccountingBirdies App <info@accountingbirdie.com>',
+    from: ' App <info@example.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
